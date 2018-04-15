2a84e8e9458332a002d3b2c7f01ca971668902a9
S. Dawson <seirradawson@Seirra-Dawsons-MacBook-Pro.local>
2018-04-15 17:08:53 -0400

    updates

---
+++ assets/javascript/gif.js
@@ -0,0 +1,30 @@
+// On click function
+$("button").on("click",function(){
+    var x = $(this).data("search");
+
+    // queryURL for gif API
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x +
+        "&api_key=Z6aJDTddb8YIRpgF6PmSR5cGXHAYSRPs&limit=10";
+        console.log(queryURL);
+
+    // AJAX call
+    $.ajax({
+        url: queryURL,
+        method: "GET"})
+        .then(function(response) {
+        console.log(response);
+
+    //Loop through your 10 images
+    for(var i = 0; i < response.data.length; i++){
+
+        var flowerDiv = $("<div>");
+        var rating = $("<p>").text("Rating: " + response.data[i].rating);
+        var flowerImg = $("<img>");
+        flowerImg.attr("src", response.data[i].images.fixed_height.url);
+
+        flowerDiv.append(rating);
+        flowerDiv.append(flowerImg);
+        $("#gifArea").prepend(flowerDiv);
+        }
+    });
+})
